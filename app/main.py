561e55e85496e24a31e84569083db33296c70f7d
Ryan Lieu <github-benzBrake@woai.ru>
2023-12-01 21:08:36 +0800

    feat: 增加从指定链接更新 blacklist，支持从环境变量读取 jsdelivr 开关、jsdelivr 镜像，监听 IP，监听端口，文件大小限制，前台静态资源链接

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+import os
 import re
-
+import socket
 import requests
 from flask import Flask, Response, redirect, request
 from requests.exceptions import (
@@ -11,33 +12,43 @@ from requests.utils import (
 from urllib3.exceptions import (
     DecodeError, ReadTimeoutError, ProtocolError)
 
-# config
-# 分支文件使用jsDelivr镜像的开关，0为关闭，默认关闭
-jsdelivr = 0
-size_limit = 1024 * 1024 * 1024 * 999  # 允许的文件大小，默认999GB，相当于无限制了 https://github.com/hunshcn/gh-proxy/issues/8
+def sanitize_mirror_url(url):
+    url = re.sub(r'^(https?://)', '', url)
+    url = re.sub(r'/$', '', url)
+    return url + '/gh'
 
-"""
-  先生效白名单再匹配黑名单，pass_list匹配到的会直接302到jsdelivr而忽略设置
-  生效顺序 白->黑->pass，可以前往https://github.com/hunshcn/gh-proxy/issues/41 查看示例
-  每个规则一行，可以封禁某个用户的所有仓库，也可以封禁某个用户的特定仓库，下方用黑名单示例，白名单同理
-  user1 # 封禁user1的所有仓库
-  user1/repo1 # 封禁user1的repo1
-  */repo1 # 封禁所有叫做repo1的仓库
-"""
-white_list = '''
-'''
-black_list = '''
-'''
-pass_list = '''
-'''
+def is_valid_ip(ip):
+    try:
+        socket.inet_pton(socket.AF_INET, ip)
+    except socket.error:
+        try:
+            socket.inet_pton(socket.AF_INET6, ip)
+        except socket.error:
+            return False
+    return True
 
-HOST = '127.0.0.1'  # 监听地址，建议监听本地然后由web服务器反代
-PORT = 80  # 监听端口
-ASSET_URL = 'https://hunshcn.github.io/gh-proxy'  # 主页
+ENV_JSDELIVR = int(os.getenv('ENABLE_JSDELIVR', 0))
+ENV_JSDELIVR_MIRROR = sanitize_mirror_url(os.getenv('SDELIVR_MIRROR', 'cdn.jsdelivr.net'))
+ENV_SIZE_LIMIT = int(os.getenv('SIZE_LIMIT', 1024 * 1024 * 1024 * 999))
+HOST = os.getenv('HOST', '127.0.0.1')
+PORT = int(os.getenv('PORT', 80))
+ASSET_URL = os.getenv('ASSET_URL', 'https://hunshcn.github.io/gh-proxy')
 
-white_list = [tuple([x.replace(' ', '') for x in i.split('/')]) for i in white_list.split('\n') if i]
-black_list = [tuple([x.replace(' ', '') for x in i.split('/')]) for i in black_list.split('\n') if i]
-pass_list = [tuple([x.replace(' ', '') for x in i.split('/')]) for i in pass_list.split('\n') if i]
+if not is_valid_ip(HOST):
+    raise ValueError(f'Invalid IP address: {HOST}')
+
+def fetch_rules_list(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return [tuple([x.replace(' ', '') for x in line.split('/')]) for line in response.text.split('\n') if line]
+    else:
+        print(f"Failed to fetch rules list from {url}. Using an empty list.")
+        return []
+
+whitelist_rules = fetch_rules_list('https://github.com/benzBrake/gh-proxy/raw/master/whitelist_rules')
+blacklist_rules = fetch_rules_list('https://github.com/benzBrake/gh-proxy/raw/master/blacklist_rules')
+passlist_rules = fetch_rules_list('https://github.com/benzBrake/gh-proxy/raw/master/passlist_rules')
+
 app = Flask(__name__)
 CHUNK_SIZE = 1024 * 10
 index_html = requests.get(ASSET_URL, timeout=10).text
@@ -50,24 +61,18 @@ exp5 = re.compile(r'^(?:https?://)?gist\.(?:githubusercontent|github)\.com/(?P<a
 
 requests.sessions.default_headers = lambda: CaseInsensitiveDict()
 
-
 @app.route('/')
 def index():
     if 'q' in request.args:
         return redirect('/' + request.args.get('q'))
     return index_html
 
-
 @app.route('/favicon.ico')
 def icon():
     return Response(icon_r, content_type='image/vnd.microsoft.icon')
 
-
 def iter_content(self, chunk_size=1, decode_unicode=False):
-    """rewrite requests function, set decode_content with False"""
-
     def generate():
-        # Special case for urllib3.
         if hasattr(self.raw, 'stream'):
             try:
                 for chunk in self.raw.stream(chunk_size, decode_content=False):
@@ -79,7 +84,6 @@ def iter_content(self, chunk_size=1, decode_unicode=False):
             except ReadTimeoutError as e:
                 raise ConnectionError(e)
         else:
-            # Standard file-like object.
             while True:
                 chunk = self.raw.read(chunk_size)
                 if not chunk:
@@ -92,18 +96,15 @@ def iter_content(self, chunk_size=1, decode_unicode=False):
         raise StreamConsumedError()
     elif chunk_size is not None and not isinstance(chunk_size, int):
         raise TypeError("chunk_size must be an int, it is instead a %s." % type(chunk_size))
-    # simulate reading small chunks of the content
+
     reused_chunks = iter_slices(self._content, chunk_size)
-
     stream_chunks = generate()
-
     chunks = reused_chunks if self._content_consumed else stream_chunks
 
     if decode_unicode:
         chunks = stream_decode_response_unicode(chunks, self)
 
     return chunks
-
 
 def check_url(u):
     for exp in (exp1, exp2, exp3, exp4, exp5):
@@ -112,39 +113,42 @@ def check_url(u):
             return m
     return False
 
-
 @app.route('/<path:u>', methods=['GET', 'POST'])
 def handler(u):
     u = u if u.startswith('http') else 'https://' + u
     if u.rfind('://', 3, 9) == -1:
-        u = u.replace('s:/', 's://', 1)  # uwsgi会将//传递为/
+        u = u.replace('s:/', 's://', 1)
+
     pass_by = False
     m = check_url(u)
+
     if m:
         m = tuple(m.groups())
-        if white_list:
-            for i in white_list:
+        if whitelist_rules:
+            for i in whitelist_rules:
                 if m[:len(i)] == i or i[0] == '*' and len(m) == 2 and m[1] == i[1]:
                     break
             else:
                 return Response('Forbidden by white list.', status=403)
-        for i in black_list:
+
+        for i in blacklist_rules:
             if m[:len(i)] == i or i[0] == '*' and len(m) == 2 and m[1] == i[1]:
                 return Response('Forbidden by black list.', status=403)
-        for i in pass_list:
+
+        for i in passlist_rules:
             if m[:len(i)] == i or i[0] == '*' and len(m) == 2 and m[1] == i[1]:
                 pass_by = True
                 break
     else:
         return Response('Invalid input.', status=403)
 
-    if (jsdelivr or pass_by) and exp2.match(u):
-        u = u.replace('/blob/', '@', 1).replace('github.com', 'cdn.jsdelivr.net/gh', 1)
+    if (ENV_JSDELIVR or pass_by) and exp2.match(u):
+        u = u.replace('/blob/', '@', 1).replace('github.com', ENV_JSDELIVR_MIRROR, 1)
         return redirect(u)
-    elif (jsdelivr or pass_by) and exp4.match(u):
+    elif (ENV_JSDELIVR or pass_by) and exp4.match(u):
         u = re.sub(r'(\.com/.*?/.+?)/(.+?/)', r'\1@\2', u, 1)
-        _u = u.replace('raw.githubusercontent.com', 'cdn.jsdelivr.net/gh', 1)
-        u = u.replace('raw.github.com', 'cdn.jsdelivr.net/gh', 1) if _u == u else _u
+        _u = u.replace('raw.githubusercontent.com', ENV_JSDELIVR_MIRROR, 1)
+        u = u.replace('raw.github.com', ENV_JSDELIVR_MIRROR, 1) if _u == u else _u
         return redirect(u)
     else:
         if exp2.match(u):
@@ -155,7 +159,6 @@ def handler(u):
                 url = 'https://' + url[7:]
             return redirect(url)
         return proxy(u)
-
 
 def proxy(u, allow_redirects=False):
     headers = {}
@@ -169,7 +172,7 @@ def proxy(u, allow_redirects=False):
         r = requests.request(method=request.method, url=url, data=request.data, headers=r_headers, stream=True, allow_redirects=allow_redirects)
         headers = dict(r.headers)
 
-        if 'Content-length' in r.headers and int(r.headers['Content-length']) > size_limit:
+        if 'Content-length' in r.headers and int(r.headers['Content-length']) > ENV_SIZE_LIMIT:
             return redirect(u + request.url.replace(request.base_url, '', 1))
 
         def generate():
